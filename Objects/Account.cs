9d33f8d93e5182a3a6b87f73737cb7d3d82821ac
Siketyan <siketyan@sikeserver.com>
2017-07-14 22:07:44 +0900

    Add DataBinding

---
+++ Objects/Account.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+namespace ClipTweet.Objects
+{
+    /// <summary>
+    /// アカウントの管理用クラス
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// プロフィール画像の周りに表示されるリングの色。
+        /// アクティブかどうかを表す
+        /// </summary>
+        [JsonIgnore]
+        public string RingColor { get; set; } = "Transperent";
+
+
+        /// <summary>
+        /// アカウントのユニークなID
+        /// </summary>
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// アカウントの名前。@~で表記されるもの
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// プロフィール画像へのURL
+        /// </summary>
+        [JsonProperty("image")]
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// アカウントにアクセスするためのトークン
+        /// </summary>
+        [JsonProperty("token")]
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// アカウントにアクセスするためのシークレット
+        /// </summary>
+        [JsonProperty("secret")]
+        public string AccessTokenSecret { get; set; }
+    }
+}
---
+++ Objects/Image.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+using Drawing = System.Drawing;
+
+namespace ClipTweet.Objects
+{
+    public class Image
+    {
+        public BitmapImage Source { get; set; }
+
+        public Image(Drawing.Image src)
+        {
+            using (var stream = new MemoryStream())
+            {
+                src.Save(stream, src.RawFormat);
+                stream.Position = 0;
+
+                this.Source = new BitmapImage();
+                this.Source.BeginInit();
+                this.Source.CacheOption = BitmapCacheOption.OnLoad;
+                this.Source.StreamSource = stream;
+                this.Source.EndInit();
+            }
+        }
+    }
+}
