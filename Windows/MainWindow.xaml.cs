391c8bcc6d5dfa923d2d45580306feef3499ca82
Siketyan <siketyan@sikeserver.com>
2017-07-14 21:56:49 +0900

    Add MainWindow

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace ClipTweet.Windows
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void OnTextFocused(object sender, RoutedEventArgs e)
+        {
+            this.placeHolder.Visibility = Visibility.Hidden;
+        }
+
+        private void OnTextUnFocused(object sender, RoutedEventArgs e)
+        {
+            if (this.text.Text == "")
+                this.placeHolder.Visibility = Visibility.Visible;
+        }
+    }
+}
