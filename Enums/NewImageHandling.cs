4c47097cfea8bbb3af5108c3910350af37141b81
Siketyan <siketyan@sikeserver.com>
2017-07-14 18:24:05 +0900

    Add settings

---
+++ Enums/NewImageHandling.cs
@@ -0,0 +1,12 @@
+﻿namespace ClipTweet.Enums
+{
+    /// <summary>
+    /// 新しい画像がクリップボードにコピーされたときの動作
+    /// </summary>
+    public enum NewImageHandling
+    {
+        AddImage, // ツイートに画像を追加
+        ClearImage, // ツイートの画像を全て削除して追加
+        NewTweet // ツイート内容を削除して追加
+    }
+}
---
+++ Objects/Settings.cs
@@ -0,0 +1,45 @@
+﻿using ClipTweet.Enums;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ClipTweet.Objects
+{
+    /// <summary>
+    /// 設定ファイル
+    /// </summary>
+    public class Settings
+    {
+        /// <summary>
+        /// 設定ファイル名
+        /// </summary>
+        [JsonIgnore]
+        private const string FILE_NAME = "settings.json";
+
+
+        /// <summary>
+        /// 新しい画像がクリップボードにコピーされたときの動作
+        /// </summary>
+        [JsonProperty("on_new_image")]
+        public NewImageHandling OnNewImage { get; set; }
+
+
+        /// <summary>
+        /// 設定ファイルを開く
+        /// </summary>
+        /// <returns>このクラス</returns>
+        public static Settings Open()
+        {
+            var json = File.ReadAllText(FILE_NAME); // JSONファイルを読み込み
+            return JsonConvert.DeserializeObject<Settings>(json); // デシリアライズ
+        }
+
+        /// <summary>
+        /// 設定ファイルを保存
+        /// </summary>
+        public void Save()
+        {
+            var json = JsonConvert.SerializeObject(this); // シリアライズ
+            File.WriteAllText(FILE_NAME, json); // JSONファイルに書き込み
+        }
+    }
+}
