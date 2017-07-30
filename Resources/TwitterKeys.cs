d6c1b875a78ec215ae1fb6170ae876d52ae46a1e
Siketyan <siketyan@sikeserver.com>
2017-07-30 19:00:05 +0900

    Add TwitterAuthWindow

---
+++ Resources/TwitterKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace ClipTweet.Resources
+{
+    public static class TwitterKeys
+    {
+        public const string CONSUMER_KEY = "demT22A42kPPXdANMWuq9ZQpA";
+        public const string CONSUMER_SECRET = "73aA4TcDDDGleFlRdaAwOqRb3p2ZMkIsUf7g9fgIsAq1ZDVXlp";
+    }
+}
---
+++ Windows/TwitterAuthWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ClipTweet.Resources;
+using CoreTweet;
+using System.Linq;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace ClipTweet.Windows
+{
+    /// <summary>
+    /// TwitterAuthWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TwitterAuthWindow : Window
+    {
+        public Tokens Result { get; private set; }
+
+        private OAuth.OAuthSession session;
+
+        public TwitterAuthWindow()
+        {
+            InitializeComponent();
+
+            this.session = OAuth.Authorize(
+                TwitterKeys.CONSUMER_KEY,
+                TwitterKeys.CONSUMER_SECRET,
+                "cliptweet://callback"
+            );
+
+            this.browser.Source = this.session.AuthorizeUri;
+        }
+
+        private async void OnNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            var uri = e.Uri;
+            if (!uri.ToString().StartsWith("cliptweet://callback")) return;
+            e.Cancel = true;
+
+            var verifier = uri.Query
+                              .Split('&')
+                              .Where(q => q.Contains("oauth_verifier")).First()
+                              .Split('=')[1];
+
+            this.Result = await this.session.GetTokensAsync(verifier);
+            Close();
+        }
+    }
+}
