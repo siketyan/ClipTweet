14e57ed6104089b6da95ef4548678b98ecffdbc5
Siketyan <siketyan@sikeserver.com>
2017-07-30 17:20:00 +0900

    Fix Image class for ClipboardWatcher

--- Objects/Image.cs
+++ Objects/Image.cs
@@ -1,26 +1,14 @@
-﻿using System.IO;
-using System.Windows.Media.Imaging;
-using Drawing = System.Drawing;
+﻿using System.Windows.Media.Imaging;
 
 namespace ClipTweet.Objects
 {
     public class Image
     {
-        public BitmapImage Source { get; set; }
+        public BitmapSource Source { get; set; }
 
-        public Image(Drawing.Image src)
+        public Image(BitmapSource src)
         {
-            using (var stream = new MemoryStream())
-            {
-                src.Save(stream, src.RawFormat);
-                stream.Position = 0;
-
-                this.Source = new BitmapImage();
-                this.Source.BeginInit();
-                this.Source.CacheOption = BitmapCacheOption.OnLoad;
-                this.Source.StreamSource = stream;
-                this.Source.EndInit();
-            }
+            this.Source = src;
         }
     }
 }
