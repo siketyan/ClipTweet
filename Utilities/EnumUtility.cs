a2df32e48c8ccdc8e387b766c4c024f58f55de47
Siketyan <siketyan@sikeserver.com>
2017-07-31 09:21:38 +0900

    Add enum utility

---
+++ Utilities/EnumUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClipTweet.Utilities
+{
+    public static class EnumUtility
+    {
+        public static T ToEnum<T>(this int i)
+        {
+            return (T)Enum.ToObject(typeof(T), i);
+        }
+    }
+}
